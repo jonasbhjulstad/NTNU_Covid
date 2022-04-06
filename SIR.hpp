fe5f32a820eb409a7ee8a531e4543f4e38c1335e
Jonas <jonasbhjulstad@gmail.com>
2022-04-06 17:02:49 +0200

    .

--- SIR.hpp
+++ SIR.hpp
@@ -11,22 +11,7 @@ template <typename InstanceData = glm::vec3>
 class glTFBasicInstance
 {
 public:
-    glTFBasicInstance(VkDevice _device, 
-    vks::VulkanDevice* _vulkanDevice, 
-    vks::Buffer* _uniformProjectionBuffer, 
-    VkQueue _queue,
-    std::string _vertexShaderPath,
-    std::string _fragmentShaderPath,
-    std::string _modelAssetPath,
-    VkRenderPass _renderPass,
-    vkglTF::Model* _model,
-    vks::Texture2DArray* _texture = nullptr)
-        : device(_device),
-          vulkanDevice(_vulkanDevice),
-          uniformProjectionBuffer(_uniformProjectionBuffer),
-          graphicsQueue(_queue),
-          renderPass(_renderPass),
-          model(_model)
+    glTFBasicInstance()
     {
     }
 
@@ -37,15 +22,6 @@ public:
     glTFBasicInstance bezier;
     glTFBasicInstance node;
     ComputeBasic compute;
-
-    constexpr std::array<VkPoolSize, 2> getPoolSizes()
-    {
-       std::vector<VkPoolSize> poolSizes;
-       poolSizes.reserve(6);
-       poolSizes.push_back(glTFBasicInstance::getPoolSizes()); 
-       poolSizes.push_back(glTFBasicInstance::getPoolSizes()); 
-       poolSizes.push_back(ComputeBasic::getPoolSizes()); 
-    }
 
     void prepare()
     {
