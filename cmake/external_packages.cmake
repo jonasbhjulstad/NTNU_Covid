bad76c032eb552a5e72873a070e3d28cdaeeb7da
jonas <jonasbhjulstad@gmail.com>
2024-05-01 03:06:13 +0200

    renderpass imgui-init

--- cmake/external_packages.cmake
+++ cmake/external_packages.cmake
@@ -1,4 +1,8 @@
-CPMFindPackage(NAME VulkanTools
-GITHUB_REPOSITORY jonasbhjulstad/CLCPP-Vulkan-Tools
-GIT_TAG master)
+include(${PROJECT_SOURCE_DIR}/cmake/CPM.cmake)
 
+
+CPMFindPackage(NAME VulkanTools
+GITHUB_REPOSITORY jonasbhjulstad/CLCPP-Vulkan-Tools)
+
+CPMFindPackage(NAME imgui
+GITHUB_REPOSITORY jonasbhjulstad/imgui)
