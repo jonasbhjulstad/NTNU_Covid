34cce49721056766abdcc214ddbc05e27bf5b8df
Jonas <jonasbhjulstad@gmail.com>
2024-05-25 11:40:06 +0200

    .

--- cmake/external_packages.cmake
+++ cmake/external_packages.cmake
@@ -2,8 +2,11 @@ include(${PROJECT_SOURCE_DIR}/cmake/CPM.cmake)
 include(FindVulkan)
 
 
-CPMFindPackage(NAME VulkanTools
-GITHUB_REPOSITORY jonasbhjulstad/CLCPP-Vulkan-Tools)
+# CPMFindPackage(NAME VulkanTools
+# GITHUB_REPOSITORY jonasbhjulstad/VulkanTools)
+find_package(glfw3 REQUIRED)
+find_package(VulkanTools REQUIRED)
+
 
 CPMFindPackage(NAME imgui
 GITHUB_REPOSITORY jonasbhjulstad/imgui)
@@ -12,6 +15,5 @@ GITHUB_REPOSITORY jonasbhjulstad/imgui)
 find_package(OpenMP REQUIRED)
 find_package(igraph REQUIRED)
 find_package(glm REQUIRED)
-find_package(glfw3 REQUIRED)
 find_package(Ktx REQUIRED)
 find_package(LAPACK REQUIRED)
